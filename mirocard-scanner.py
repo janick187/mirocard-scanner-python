1a032157fc2d8a6b84981837e2c7891cfe616cb4
Andres Gomez <andres@mirocard.swiss>
2021-05-21 11:44:02 +0200

    initial commit

---
+++ mirocard-scanner.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+from __future__ import print_function
+
+from time import gmtime, strftime, sleep
+from bluepy.btle import Scanner, DefaultDelegate, BTLEException
+import struct
+from base64 import b64decode,b64encode
+import sys
+
+
+class ScanDelegate(DefaultDelegate):
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if(str(dev.addr).startswith("60:77:71")):
+            print(strftime("%Y-%m-%d %H:%M:%S", gmtime()), dev.addr, dev.getScanData()[0][2])
+            rawbytes = str(dev.getScanData()[0][2])
+
+            msb3 = rawbytes[10:14]
+            lsb3 = rawbytes[12:16]
+
+            # print("MSB: {} \t LSB: {}".format(msb3,lsb3))
+
+            humidity_raw = ((struct.unpack('<b', bytes.fromhex(msb3[2:]))[0]&0x03)<<8) + struct.unpack('<b', bytes.fromhex(msb3[0:2]))[0]
+
+            temperature_raw = ((struct.unpack('<b', bytes.fromhex(lsb3[0:2]))[0]&0xFC)>>2) + (struct.unpack('<b', bytes.fromhex(lsb3[2:]))[0]<<6)
+
+            # print("Raw H: {} \t Raw T: {}".format(humidity_raw, temperature_raw))
+
+            temperature = (-40.0 + temperature_raw / 100.0)
+            humidity =  (humidity_raw / 10.0)
+
+            print("H: {:.3f}% \t T: {:.3f} C".format(humidity, temperature))
+
+            sys.stdout.flush()
+
+scanner = Scanner().withDelegate(ScanDelegate())
+
+# listen for ADV_IND packages for 10s, then exit
+scanner.scan(0, passive=True)
---
+++ readme.md
@@ -0,0 +1,22 @@
+mirocard-scanner-python
+===============
+
+This repository contains simple python3 scripts to detect MiroCard beacons.
+
+## Dependencies
+
+* Python3
+* [pybluez](https://github.com/pybluez/pybluez)
+
+For Python 3, you may need to use `pip3`:
+
+```
+$ sudo apt-get install python3-pip libglib2.0-dev
+$ sudo pip3 install bluepy
+```
+
+After installing, you can run the following python script:
+
+```
+$ sudo python3 mirocard-scanner.py
+```
